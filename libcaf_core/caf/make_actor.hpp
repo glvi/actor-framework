afebce480f7b81414b122be6b52c8022ff864c83
Dominik Charousset <dominik@charousset.de>
2020-04-07 16:35:54 +0200

    Set correct actor ID before running constructors

--- libcaf_core/caf/make_actor.hpp
+++ libcaf_core/caf/make_actor.hpp
@@ -20,37 +20,37 @@
 
 #include <type_traits>
 
+#include "caf/actor_control_block.hpp"
+#include "caf/actor_storage.hpp"
 #include "caf/fwd.hpp"
-#include "caf/logger.hpp"
-#include "caf/ref_counted.hpp"
 #include "caf/infer_handle.hpp"
 #include "caf/intrusive_ptr.hpp"
-#include "caf/actor_storage.hpp"
-#include "caf/actor_control_block.hpp"
+#include "caf/logger.hpp"
+#include "caf/ref_counted.hpp"
 
 namespace caf {
 
 template <class T, class R = infer_handle_from_class_t<T>, class... Ts>
 R make_actor(actor_id aid, node_id nid, actor_system* sys, Ts&&... xs) {
 #if CAF_LOG_LEVEL >= CAF_LOG_LEVEL_DEBUG
-  actor_storage<T>* ptr = nullptr;
   if (logger::current_logger()->accepts(CAF_LOG_LEVEL_DEBUG,
                                         caf::atom(CAF_LOG_FLOW_COMPONENT))) {
     std::string args;
     args = deep_to_string(std::forward_as_tuple(xs...));
-    ptr = new actor_storage<T>(aid, std::move(nid), sys,
-                               std::forward<Ts>(xs)...);
+    actor_storage<T>* ptr;
+    {
+      CAF_PUSH_AID(aid);
+      ptr = new actor_storage<T>(aid, std::move(nid), sys,
+                                 std::forward<Ts>(xs)...);
+    }
     CAF_LOG_SPAWN_EVENT(ptr->data, args);
-  } else {
-    ptr = new actor_storage<T>(aid, std::move(nid), sys,
-                               std::forward<Ts>(xs)...);
+    return {&(ptr->ctrl), false};
   }
-#else
+#endif
+  CAF_PUSH_AID(aid);
   auto ptr = new actor_storage<T>(aid, std::move(nid), sys,
                                   std::forward<Ts>(xs)...);
-#endif
   return {&(ptr->ctrl), false};
 }
 
 } // namespace caf
-
